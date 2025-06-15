59a5594e564c946766d48ff54bf8c545a8182bc9
Lucas Linhares <lucasdelinhares@gmail.com>
2025-06-15 21:51:47 +0100

    remove enigo dependency

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-use std::cell::RefCell;
-
-use enigo::{Enigo, Keyboard, Settings};
-use rdev::{Event, EventType, Key, grab};
-
-thread_local! {
-    static ENIGO: RefCell<Enigo> = RefCell::new(unsafe {
-        Enigo::new(&Settings::default()).unwrap_unchecked()
-    });
-}
+use rdev::{Event, EventType, Key, grab, simulate};
 
 fn main() {
     println!("Listening");
@@ -20,34 +11,70 @@ fn main() {
     }
 }
 
+fn text(text: &str) {
+    for c in text.chars() {
+        unsafe {
+            let key = match c {
+                'a' => Key::KeyA,
+                'b' => Key::KeyB,
+                'c' => Key::KeyC,
+                'd' => Key::KeyD,
+                'e' => Key::KeyE,
+                'f' => Key::KeyF,
+                'g' => Key::KeyG,
+                'h' => Key::KeyH,
+                'i' => Key::KeyI,
+                'j' => Key::KeyJ,
+                'k' => Key::KeyK,
+                'l' => Key::KeyL,
+                'm' => Key::KeyM,
+                'n' => Key::KeyN,
+                'o' => Key::KeyO,
+                'p' => Key::KeyP,
+                'q' => Key::KeyQ,
+                'r' => Key::KeyR,
+                's' => Key::KeyS,
+                't' => Key::KeyT,
+                'u' => Key::KeyU,
+                'v' => Key::KeyV,
+                'w' => Key::KeyW,
+                'x' => Key::KeyX,
+                'y' => Key::KeyY,
+                'z' => Key::KeyZ,
+                '0' => Key::Num0,
+                '1' => Key::Num1,
+                '2' => Key::Num2,
+                '3' => Key::Num3,
+                '4' => Key::Num4,
+                '5' => Key::Num5,
+                '6' => Key::Num6,
+                '7' => Key::Num7,
+                '8' => Key::Num8,
+                '9' => Key::Num9,
+                ' ' => Key::Space,
+                '\n' => Key::Return,
+                _ => continue,
+            };
+            simulate(&EventType::KeyPress(key)).unwrap_unchecked();
+        }
+    }
+}
+
 fn callback(event: Event) -> Option<Event> {
     unsafe {
         match event.event_type {
             EventType::KeyPress(Key::Kp6) => {
-                ENIGO.with(|enigo| {
-                    let mut enigo = enigo.borrow_mut();
-                    enigo.text("switch").unwrap_unchecked();
-                    enigo
-                        .key(enigo::Key::Return, enigo::Direction::Click)
-                        .unwrap_unchecked();
-                });
+                text("switch");
+                simulate(&EventType::KeyPress(Key::Return)).unwrap_unchecked();
                 return None;
             }
             EventType::KeyPress(Key::Kp5) => {
-                ENIGO.with(|enigo| {
-                    let mut enigo = enigo.borrow_mut();
-                    enigo.text("view monitor").unwrap_unchecked();
-                    enigo
-                        .key(enigo::Key::Return, enigo::Direction::Click)
-                        .unwrap_unchecked();
-                });
+                text("view monitor");
+                simulate(&EventType::KeyPress(Key::Return)).unwrap_unchecked();
                 return None;
             }
             EventType::KeyPress(Key::Num5) => {
-                ENIGO.with(|enigo| {
-                    let mut enigo = enigo.borrow_mut();
-                    enigo.text("transmit ").unwrap_unchecked();
-                });
+                text("transmit ");
                 return None;
             }
             _ => (),
